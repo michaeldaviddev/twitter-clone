b8e4bbc58ed26bab4aa75c500f23215c1afc6ac8
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-09-05 23:29:49 -0500

    Delete unused tabs

--- src/app/pages/tabs/tabs.routes.ts
+++ src/app/pages/tabs/tabs.routes.ts
@@ -32,21 +32,6 @@ export const routes: Routes = [
           import('./messages/messages.page').then( m => m.MessagesPage)
       },
       {
-        path: 'tab1',
-        loadComponent: () =>
-          import('../../tab1/tab1.page').then((m) => m.Tab1Page),
-      },
-      {
-        path: 'tab2',
-        loadComponent: () =>
-          import('../../tab2/tab2.page').then((m) => m.Tab2Page),
-      },
-      {
-        path: 'tab3',
-        loadComponent: () =>
-          import('../../tab3/tab3.page').then((m) => m.Tab3Page),
-      },
-      {
         path: '',
         redirectTo: '/tabs/home',
         pathMatch: 'full',
