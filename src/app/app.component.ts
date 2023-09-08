2106fcfa782bcf4a8739cc61d3ea31b463e84558
Michael David Machacuay <mdavidmachacuay@gmail.com>
2023-09-07 21:29:04 -0500

    Add options to side menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,26 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule],
+  imports: [IonicModule, RouterLink, CommonModule],
 })
 export class AppComponent {
   public environmentInjector = inject(EnvironmentInjector);
+
+  public appPages = [
+    { title: 'Profile', url: '/', icon: 'person' },
+    { title: 'Premium', url: '/folder/outbox', icon: 'person' },
+    { title: 'Bookmarks', url: '/folder/favorites', icon: 'bookmark' },
+    { title: 'Lists', url: '/folder/archived', icon: 'reader' },
+    { title: 'Spaces', url: '/folder/trash', icon: 'mic' },
+    { title: 'Monetization', url: '/folder/spam', icon: 'cash' },
+  ];
 
   constructor() {}
 }
